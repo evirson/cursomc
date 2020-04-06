4656226041cfdae6d88d71e45c04748c3cb21c34
evirson <evirson@vetorsistemas.com.br>
2020-04-05 21:14:06 -0300

    Estados e Cidades

--- src/main/java/br/com/esoft/cursomc/CursomcApplication.java
+++ src/main/java/br/com/esoft/cursomc/CursomcApplication.java
@@ -8,20 +8,29 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import br.com.esoft.cursomc.domain.Categoria;
+import br.com.esoft.cursomc.domain.Cidade;
+import br.com.esoft.cursomc.domain.Estado;
 import br.com.esoft.cursomc.domain.Produto;
 import br.com.esoft.cursomc.repositories.CategoriaRepository;
+import br.com.esoft.cursomc.repositories.CidadeRepository;
+import br.com.esoft.cursomc.repositories.EstadoRepository;
 import br.com.esoft.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
-public class CursomcApplication implements CommandLineRunner{
+public class CursomcApplication implements CommandLineRunner {
 
 	@Autowired
 	CategoriaRepository categoriaRepository;
-	
+
 	@Autowired
 	ProdutoRepository produtoRepository;
-	
-	
+
+	@Autowired
+	CidadeRepository cidadeRepository;
+
+	@Autowired
+	EstadoRepository estadoRepository;
+
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
 	}
@@ -31,22 +40,35 @@ public class CursomcApplication implements CommandLineRunner{
 		Categoria cat1 = new Categoria(null, "Informática");
 
 		Categoria cat2 = new Categoria(null, "Escritório");
-		
+
 		Produto p1 = new Produto(null, "Computador", 2000);
 		Produto p2 = new Produto(null, "Impressora", 800);
 		Produto p3 = new Produto(null, "Mouse", 80);
-		
-		/*associacoes muitos pra muitos*/
+
+		/* associacoes muitos pra muitos */
 		cat1.getProdutos().addAll(Arrays.asList(p1, p3));
 		cat2.getProdutos().addAll(Arrays.asList(p2));
-		
+
 		p1.getCategorias().addAll(Arrays.asList(cat1));
 		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
 		p3.getCategorias().addAll(Arrays.asList(cat1));
-		
+
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
+
+		Estado est1 = new Estado(null, "Minas Gerais");
+		Estado est2 = new Estado(null, "São Paulo");
+
+		Cidade c1 = new Cidade(null, "Uberlandia", est1);
+		Cidade c2 = new Cidade(null, "São Paulo", est2);
+		Cidade c3 = new Cidade(null, "Campinas", est2);
+
+		est1.getCidades().addAll(Arrays.asList(c1));
+		est2.getCidades().addAll(Arrays.asList(c2, c3));
 		
+		estadoRepository.saveAll(Arrays.asList(est1, est2));
+		cidadeRepository.saveAll(Arrays.asList(c1,c2,c3));
+
 	}
 
 }
--- src/main/java/br/com/esoft/cursomc/domain/Estado.java
+++ src/main/java/br/com/esoft/cursomc/domain/Estado.java
@@ -4,34 +4,29 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.ManyToMany;
-
-import com.fasterxml.jackson.annotation.JsonManagedReference;
+import javax.persistence.OneToMany;
 
 @Entity
-public class Categoria implements Serializable {
+public class Estado implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
-	@Column(name = "nome")
 	private String nome;
 	
-	@JsonManagedReference  /*faz do lado que vc quer que venham os produtos */
-	@ManyToMany(mappedBy = "categorias")  /*vem lá da List de categorias (Categoria)*/
-	private List<Produto> produtos = new ArrayList<>();
+	@OneToMany(mappedBy="estado")
+	private List<Cidade> cidades = new ArrayList<>();
 
-	public Categoria() {
+	public Estado() {
 
 	}
 
-	public Categoria(Integer id, String nome) {
+	public Estado(Integer id, String nome) {
 		super();
 		this.id = id;
 		this.nome = nome;
@@ -53,12 +48,12 @@ public class Categoria implements Serializable {
 		this.nome = nome;
 	}
 
-	public List<Produto> getProdutos() {
-		return produtos;
+	public List<Cidade> getCidades() {
+		return cidades;
 	}
 
-	public void setProdutos(List<Produto> produtos) {
-		this.produtos = produtos;
+	public void setCidades(List<Cidade> cidades) {
+		this.cidades = cidades;
 	}
 
 	@Override
@@ -77,7 +72,7 @@ public class Categoria implements Serializable {
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
-		Categoria other = (Categoria) obj;
+		Estado other = (Estado) obj;
 		if (id == null) {
 			if (other.id != null)
 				return false;
